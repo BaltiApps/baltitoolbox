c316e3c97866fe9ee6bc4133da5613874202a8b6
SayantanRC <sayantan_rc@protonmail.com>
2022-03-27 05:27:43 +0530

    GetResources - update code to use application instead of context. add method getStringFromRes(id: Int, vararg formatArgs: Any).

--- src/main/java/balti/module/baltitoolbox/functions/GetResources.kt
+++ src/main/java/balti/module/baltitoolbox/functions/GetResources.kt
@@ -1,23 +1,23 @@
 package balti.module.baltitoolbox.functions
 
-import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.widget.ImageViewCompat
 import balti.module.baltitoolbox.ToolboxHQ
 
-@SuppressLint("StaticFieldLeak")
 object GetResources {
 
-    private val context = ToolboxHQ.context
+    private val application = ToolboxHQ.application
 
-    fun getStringFromRes(id: Int): String = context.getString(id)
-    fun getDrawableFromRes(id: Int): Drawable? = context.getDrawable(id)
-    fun getColorFromRes(id: Int): Int = ContextCompat.getColor(context, id)
+    fun getStringFromRes(id: Int): String = application.getString(id)
+    fun getStringFromRes(id: Int, vararg formatArgs: Any): String = application.getString(id, formatArgs)
+    fun getDrawableFromRes(id: Int): Drawable? = AppCompatResources.getDrawable(application, id)
+    fun getColorFromRes(id: Int): Int = ContextCompat.getColor(application, id)
 
     fun setTintFromRes(view: View, rColor: Int) {
         ViewCompat.setBackgroundTintList(view, ColorStateList.valueOf(getColorFromRes(rColor)))
